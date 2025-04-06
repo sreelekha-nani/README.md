780ddd4df86d0e300f75b6efb0f391fcacd57625
SREELEKHA BAYAM <bayamsreelekha99@gmail.com>
2025-04-06 18:53:54 +0530

    Add files via upload

---
+++ create a personalised greeting.py
@@ -0,0 +1,4 @@
+a=input("Enter your first name:")
+b=input("Enter your last name:")
+
+print("Hello, "+ (a+b) + "!" "welcome to the python program")
---
+++ mathematical operations.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
